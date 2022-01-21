364fd40e54696d432233092594ba00b3ffdea1b0
Ben <benboorstein@gmail.com>
2022-01-21 15:30:42 -0500

    initial commit for Frontend Mentor project eleven

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+/* From FEM:
+Your users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Create, Read, Update, and Delete comments and replies
+- Upvote and downvote comments
+- Bonus: If you're building a purely front-end project, use localStorage to save the current state in the browser that persists when the browser is refreshed.
+*/
+
+
+
+
+
+
+
+
+
+// Next FEM project I think: https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29
